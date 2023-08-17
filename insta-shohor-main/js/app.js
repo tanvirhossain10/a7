3868c9af3389c5e8c5f9cc17118d9e478740fd58
tanvirhossain10 <tanvirhossen808@gmail.com>
2023-08-17 17:21:46 +0600

    At last able to fix bugs no 5 showing only reported image need namaj break

--- insta-shohor-main/js/app.js
+++ insta-shohor-main/js/app.js
@@ -151,10 +151,10 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
   const reportedPosts = getReportedPosts();
-  posts.forEach((post) => {
+  reportedPosts.forEach(post => {
     const div = createPost(post);
     document.getElementById("reported").appendChild(div);
-  });
+  })
 };
 
 const loadPosts = async () => {
